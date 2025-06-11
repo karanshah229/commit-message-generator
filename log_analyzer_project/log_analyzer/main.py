9b52052b8277f2335cc42a581d01d35fb191dff6
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2025-06-11 17:48:23 +0530

    feat: Add status code distribution feature
    - Analyzer module updated to include a method for calculating the distribution of HTTP status codes.
    - Report module updated to print the status code distribution.

--- log_analyzer_project/log_analyzer/main.py
+++ log_analyzer_project/log_analyzer/main.py
@@ -1,7 +1,7 @@
 import sys
 from log_analyzer.log_parser import read_log_file
-from log_analyzer.analyzer import most_requested_endpoints
-from log_analyzer.report import print_endpoint_stats
+from log_analyzer.analyzer import most_requested_endpoints, status_code_distribution
+from log_analyzer.report import print_endpoint_stats, print_status_codes
 
 def main():
     import sys
@@ -13,8 +13,10 @@ def main():
     logs = list(read_log_file(log_file_path))
 
     endpoint_stats = most_requested_endpoints(logs)
+    status_codes = status_code_distribution(logs)
 
     print_endpoint_stats(endpoint_stats)
+    print_status_codes(status_codes)
 
 
 if __name__ == "__main__":
