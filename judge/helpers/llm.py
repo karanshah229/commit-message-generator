f3ac395fab1cd3bfde1b63e74a178079fa386c3e
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2025-06-21 20:32:10 +0530

    chore: Use OPENAI_API_KEY from env

--- judge/helpers/llm.py
+++ judge/helpers/llm.py
@@ -4,19 +4,20 @@ import subprocess
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from mcp_use import MCPAgent, MCPClient
-from portkey_ai import createHeaders, PORTKEY_GATEWAY_URL
 
 load_dotenv(os.path.join(os.path.dirname(__file__), "..", "..", ".env.local"))
-PORTKEY_API_KEY = os.getenv("PORTKEY_API_KEY")
-VIRTUAL_KEY = os.getenv("PORTKEY_VIRTUAL_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-def get_portkey_langchain_llm():
-    portkey_headers = createHeaders(api_key=PORTKEY_API_KEY, virtual_key=VIRTUAL_KEY)
-
-    return ChatOpenAI(api_key="X", base_url=PORTKEY_GATEWAY_URL, default_headers=portkey_headers, model="gpt-4o-mini", temperature=0.1)
+def get_llm():
+    return ChatOpenAI(
+      api_key=OPENAI_API_KEY,
+      temperature=0,
+      model="gpt-4o",
+      max_retries=5,
+  )
 
 def get_mcp_client():
-    llm = get_portkey_langchain_llm()
+    llm = get_llm()
     current_dir = os.path.dirname(__file__)
     kanban_server_path = os.path.join(current_dir, '..', '..', 'kanban', 'main.py')
 
